a37946827cb0f36126abde09d61d47eace71470d
Farhad Athar <jamfarhadathar@gmail.com>
2025-02-05 20:13:37 +0500

    api response

--- lib/api_services/partial_deck_api.dart
+++ lib/api_services/partial_deck_api.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_deck_of_cards/api_services/api_services_template.dart';
 import 'package:flutter_deck_of_cards/model_classes/partial_deck.dart';
 
-class ReShuffleCardsApi extends ApiService {
+class PartialDeckApi extends ApiService {
   @override
   String get apiURL => 'new/shuffle/?cards=AS,2S,KS,AD,2D,KD,AC,2C,KC,AH,2H,KH';
 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_deck_of_cards/api_services/adding_piles_api.dart';
+import 'package:flutter_deck_of_cards/api_services/draw_card_api.dart';
+import 'package:flutter_deck_of_cards/api_services/drawing_from_piles.dart';
+import 'package:flutter_deck_of_cards/api_services/listing_cards_api.dart';
+import 'package:flutter_deck_of_cards/api_services/new_deck_api.dart';
+import 'package:flutter_deck_of_cards/api_services/partial_deck_api.dart';
+import 'package:flutter_deck_of_cards/api_services/reshuffle_cards_api.dart';
+import 'package:flutter_deck_of_cards/api_services/returning_card.dart';
+import 'package:flutter_deck_of_cards/api_services/shuffle_api.dart';
+import 'package:flutter_deck_of_cards/api_services/suffle_piles_api.dart';
+import 'package:flutter_deck_of_cards/model_classes/add_piles.dart';
+import 'package:flutter_deck_of_cards/model_classes/draw_card.dart';
+import 'package:flutter_deck_of_cards/model_classes/drwing_from_piles.dart';
+import 'package:flutter_deck_of_cards/model_classes/list_cards_pile.dart';
+import 'package:flutter_deck_of_cards/model_classes/new_deck.dart';
+import 'package:flutter_deck_of_cards/model_classes/partial_deck.dart';
+import 'package:flutter_deck_of_cards/model_classes/reshuffle_cards.dart';
+import 'package:flutter_deck_of_cards/model_classes/return_cards_to_deck.dart';
+import 'package:flutter_deck_of_cards/model_classes/shuffle_cards.dart';
+import 'package:flutter_deck_of_cards/model_classes/shuffle_piles.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -10,14 +30,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  AddingPilesApi addingPilesApi = AddingPilesApi();
+  Future<AddPiles>? addingPilesFuture;
+  DrawCardApi drawCardApi = DrawCardApi();
+  Future<DrawCard>? drawCardFuture;
+  DrawingFromPilesApi drawingFromPilesApi = DrawingFromPilesApi();
+  Future<DrawingFromPiles>? drawingFromPilesFuture;
+  ListingCardsApi listingCardsApi = ListingCardsApi();
+  Future<ListingCardPiles>? listingCardsFuture;
+  NewDeckApi newDeckApi = NewDeckApi();
+  Future<NewDeck>? newDeckFuture;
+  PartialDeckApi partialDeckApi = PartialDeckApi();
+  Future<PartialDeck>? partialDeckFuture;
+  ReShuffleCardsApi reShuffleCardsApi = ReShuffleCardsApi();
+  Future<ReShuffleCards>? reShuffleCardsFuture;
+  ReturningCardApi returningCardApi = ReturningCardApi();
+  Future<ReturningCardsToDeck>? returningCardFuture;
+  ShuffleCardsApi shuffleCardsApi = ShuffleCardsApi();
+  Future<ShuffleCards>? shuffleCardsFuture;
+  ShufflePilesApi shufflePilesApi = ShufflePilesApi();
+  Future<ShufflePiles>? shufflePilesFuture;
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 249, 227, 119),
           title: Text(widget.title,
-              style:
-                  const TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.black)),
           centerTitle: true,
         ),
         body: Expanded(
@@ -49,7 +91,11 @@ class _MyHomePageState extends State<MyHomePage> {
                                     backgroundColor:
                                         MaterialStateProperty.all<Color>(
                                             const Color.fromARGB(
-                                                255, 249, 227, 119))),
+                                                255, 249, 227, 119)),
+                                    elevation:
+                                        MaterialStateProperty.all<double>(10.0),
+                                    shadowColor: const MaterialStatePropertyAll(
+                                        Colors.white)),
                                 onPressed: () {
                                   // Navigator.pushNamed(context, '/new_deck');
                                 },
